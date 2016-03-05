e0c4fc4d87f0c8e543f5a9b4579508670963da3d
Magda Boron <magdabo@gmail.com>
2016-03-05 14:05:21 +0100

    Modified templates, Added tag views

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     url(r'^new_link/$', views.new_link, name='new_link'),
     url(r'^new_photo/$', views.new_photo, name='new_photo'),
     url(r'^post_list/$', views.post_list, name='post_list'),
+    url(r'^post/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^tags/(?P<tag_id>[a-zA-Z0-9_.-]+)/$', views.tag_detail, name='tag_detail'),
+    url(r'^tags_list/$', views.tags_list, name='tags_list'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,20 +1,42 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+#from example_data.py import posts
+
+tags = ['restaurant', 'hostel', 'highlight', 'nature']
+
 
 def index(request):
-    return HttpResponse("Hello, world. This is the main site of my travel app")
+    return render(request, 'main/index.html', {})
+
 
 def new_note(request):
-    return HttpResponse("Here you can add a new note")
+    return render(request, 'main/new_note.html', {})
+
 
 def new_link(request):
-    return HttpResponse("Here you can add a new link")
+    return render(request, 'main/new_link.html', {})
+
 
 def new_photo(request):
-    return HttpResponse("Add a new photo here")
+    return render(request, 'main/new_photo.html', {})
+
 
 def post_list(request):
-    return HttpResponse("list of all posts")
+    return render(request, 'main/post_list.html', {})
 
 
-# Create your views here.
+def post_detail(request, post_id):
+    return HttpResponse('This is a post %s.' % post_id)
+
+
+def tag_detail(request, tag_id):
+    if str(tag_id).lower() in tags:
+        return HttpResponse('here is a list of tags for %s tag.' % tag_id)
+    else:
+        raise Http404("This tag does not exist!!!!")
+
+
+def tags_list(request):
+    all_tags = tags
+    return render(request, 'main/tags_list.html', {})
